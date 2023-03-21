039ff78c3f57ae4d79afa966bd68de6f5a579e55
Diogo Gaspar <hackerman372@protonmail.com>
2023-03-21 12:06:43 +0000

    Add @vercel/analytics dependency and inject it in +layout.svelte for development and production mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
+
+
 <script>
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 	import Nav from '../components/Nav.svelte';
 	import '../app.css';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Nav />
